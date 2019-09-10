ea6a8921f90087a984ab114d3c1f3859463ff1f2
YauhenTaishyn <yauhen1990@gmail.com>
2019-09-10 15:26:51 +0300

    coonect

---
+++ src/components/users/Users.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+let Users =(props)=>{
+    return <div> users here </div>
+};
+export default Users;
---
+++ src/redux/users-reducer.js
@@ -0,0 +1,45 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+
+let initialState ={
+    posts: [
+        {id: 1, message: 'hello, how are you ?', likesCount: 10},
+        {id: 2, message: 'I am fine !', likesCount: 2},
+    ],
+    newPostText: 'hello world'
+};
+
+
+
+ const profileReducer  = (state = initialState,action) => {
+     switch (action.type) {
+         case ADD_POST : {
+             let newPost = {
+                 id: 5,
+                 message: state.newPostText,
+                 likesCount: 0
+             };
+             return  {
+                 ...state,
+                 posts : [...state.posts, newPost],
+                 newPostText : ''
+             };
+         }
+         case UPDATE_NEW_POST_TEXT: {
+             return  {
+                 ...state,
+                 newPostText : action.newText
+             };
+         }
+     }
+
+    return state;
+};
+
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text
+})
+ export default profileReducer;
